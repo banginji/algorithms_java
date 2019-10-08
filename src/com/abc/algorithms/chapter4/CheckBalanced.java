bf1d3b4e94a77e3830e23a97165c3f0841f43453
Narayanan Lakshmi <narayanan.lakshmi@tr.com>
2019-10-08 12:54:14 -0500

    completing balance check implementation

--- src/com/abc/algorithms/chapter4/CheckBalanced.java
+++ src/com/abc/algorithms/chapter4/CheckBalanced.java
@@ -7,29 +7,39 @@ public class CheckBalanced {
 	public static void main(String[] args) {
 		System.out.println("Checking if binary tree is balanced...");
 
-		checkBalanced();
+		if(checkBalanced()) {
+			System.out.println("The binary tree is balanced");
+		} else {
+			System.out.println("The binary tree is NOT balanced");
+		}
 	}
 
-	private static void checkBalanced() {
+	private static boolean checkBalanced() {
 		Node<Integer> rootNode = binaryTree();
 
 		System.out.println("Left height: " + height(rootNode.left));
 		System.out.println("Right height: " + height(rootNode.right));
+
+		return Math.abs(height(rootNode.left) - height(rootNode.right)) <= 1;
 	}
 
 	private static int height(Node node) {
-		int height = 0;
+		int leftHeight = -1, rightHeight = -1;
 		
 		if (node.left != null) {
-			height += height(node.left);
-			height++;
+			leftHeight = height(node.left);
+			leftHeight++;
+		} else {
+			leftHeight++;
 		}
 
 		if (node.right != null) {
-			height += height(node.right);
-			height++;
+			rightHeight = height(node.right);
+			rightHeight++;
+		} else {
+			rightHeight++;
 		}
 
-		return height;
+		return Math.max(leftHeight, rightHeight);
 	}
 }
