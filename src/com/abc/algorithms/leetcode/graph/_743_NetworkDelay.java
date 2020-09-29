876a989592b222b6119e6deb38c754e6f89f245b
Narayanan Lakshmi <narayanan.lakshmi@tr.com>
2020-09-29 03:49:19 -0500

    743 - nw delay modification leet soln

--- src/com/abc/algorithms/leetcode/graph/_743_NetworkDelay.java
+++ src/com/abc/algorithms/leetcode/graph/_743_NetworkDelay.java
@@ -4,34 +4,46 @@ import java.util.*;
 
 public class _743_NetworkDelay {
     private static int networkDelay(int[][] times, int n, int k) {
-        Map<Integer, List<int[]>> neighbors = new HashMap<>();
+        Map<Integer, List<int[]>> neighborMap = new HashMap<>();
 
-        for (int node = 1; node <= n; node++)
-            neighbors.put(node, new ArrayList<>());
+        for (int[] time: times)
+            neighborMap.computeIfAbsent(time[0], x -> new ArrayList<>()).add(new int[]{time[1], time[2]});
 
-        for (int[] time : times)
-            neighbors.computeIfPresent(time[0], (key, value) -> {
-                value.add(new int[]{time[1], time[2]});
-                return value;
-            });
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(nodeAndWeight -> nodeAndWeight[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a , b) -> b[1] - a[1]);
         pq.offer(new int[]{k, 0});
 
+        int time = Integer.MIN_VALUE;
+
+        Set<Integer> visited = new HashSet<>();
+
         while (pq.size() > 0) {
-            int[] currentNodeAndWeight = pq.poll();
+            /**
+             * If all nodes are visited and pq is not empty it means that there is a cycle
+             * Break the loop and return the time
+             */
+            if (visited.size() == n)
+                break;
 
-            List<int[]> currentNodeNeighbors = neighbors.remove(currentNodeAndWeight[0]);
+            int[] currentNodeAndTime = pq.poll();
+            int currentNode = currentNodeAndTime[0];
+            int currentTime = currentNodeAndTime[1];
 
-            if (neighbors.isEmpty()) return currentNodeAndWeight[1];
+            /**
+             * If there is a smaller cycle with the source node, it means not all nodes are
+             * reachable. Hence return -1 else add node to visited set
+             */
+            if (visited.contains(currentNode))
+                return -1;
+            else
+                visited.add(currentNode);
 
-            if (currentNodeNeighbors == null) continue;
+            time = Math.max(time, currentTime);
 
-            for (int[] neighbor : currentNodeNeighbors)
-                pq.offer(new int[]{neighbor[0], neighbor[1] + currentNodeAndWeight[1]});
+            for (int[] neighbor: neighborMap.getOrDefault(currentNode, Collections.emptyList()))
+                pq.offer(new int[]{neighbor[0], currentTime + neighbor[1]});
         }
 
-        return -1;
+        return visited.size() == n ? time : -1;
     }
 
     public static void main(String[] args) {
@@ -44,7 +56,60 @@ public class _743_NetworkDelay {
                         },
                         3,
                         1
-                )
+                ) == 4
+        );
+
+        System.out.println(
+                networkDelay(
+                        new int[][]{
+                                new int[]{2, 1, 1},
+                                new int[]{2, 3, 1},
+                                new int[]{3, 4, 1}
+                        },
+                        4,
+                        2
+                ) == 2
+        );
+
+        // Node 4 is not reachable from 2
+        System.out.println(
+                networkDelay(
+                        new int[][]{
+                                new int[]{2, 1, 1},
+                                new int[]{2, 3, 1},
+                                new int[]{4, 3, 1}
+                        },
+                        4,
+                        2
+                ) == -1
+        );
+
+        // Cycle is present but all nodes were traversed
+        System.out.println(
+                networkDelay(
+                        new int[][]{
+                                new int[]{2, 1, 1},
+                                new int[]{2, 3, 1},
+                                new int[]{3, 4, 1},
+                                new int[]{4, 2, 1}
+                        },
+                        4,
+                        2
+                ) == 2
+        );
+
+        // Cycle is present but NOT all nodes were traversed
+        System.out.println(
+                networkDelay(
+                        new int[][]{
+                                new int[]{2, 1, 1},
+                                new int[]{1, 2, 1},
+                                new int[]{2, 3, 1},
+                                new int[]{3, 4, 1}
+                        },
+                        4,
+                        2
+                ) == -1
         );
     }
 }
