090a50560caa0ec305c1e8481ca8074552c2d11e
Narayanan Lakshmi <narayanan.lakshmi@tr.com>
2020-10-11 15:51:42 -0700

    53 - max sum sub arr leet soln

---
+++ src/com/abc/algorithms/leetcode/hashtable/_53_MaxSumSubArray.java
@@ -0,0 +1,88 @@
+package com.abc.algorithms.leetcode.hashtable;
+
+public class _53_MaxSumSubArray {
+    private static int maxSum(int[] nums) {
+        int maxSum = nums[0], currentMaxSum = nums[0];
+
+        for (int idx = 1; idx < nums.length; idx++) {
+            currentMaxSum = Math.max(nums[idx], currentMaxSum + nums[idx]);
+            maxSum = Math.max(maxSum, currentMaxSum);
+        }
+
+        return maxSum;
+    }
+
+    private static int maxSumQuadComp(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int iIdx = 0; iIdx < nums.length; iIdx++) {
+            int sum = 0;
+            for (int jIdx = iIdx; jIdx < nums.length; jIdx++)
+                maxSum = Math.max(sum += nums[jIdx], maxSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                maxSum(
+                        new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+                ) == 6
+        );
+
+        System.out.println(
+                maxSum(
+                        new int[]{1}
+                ) == 1
+        );
+
+        System.out.println(
+                maxSum(
+                        new int[]{0}
+                ) == 0
+        );
+
+        System.out.println(
+                maxSum(
+                        new int[]{-1}
+                ) == -1
+        );
+
+        System.out.println(
+                maxSum(
+                        new int[]{-2147483647}
+                ) == -2147483647
+        );
+
+        System.out.println(
+                maxSumQuadComp(
+                        new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+                ) == 6
+        );
+
+        System.out.println(
+                maxSumQuadComp(
+                        new int[]{1}
+                ) == 1
+        );
+
+        System.out.println(
+                maxSumQuadComp(
+                        new int[]{0}
+                ) == 0
+        );
+
+        System.out.println(
+                maxSumQuadComp(
+                        new int[]{-1}
+                ) == -1
+        );
+
+        System.out.println(
+                maxSumQuadComp(
+                        new int[]{-2147483647}
+                ) == -2147483647
+        );
+    }
+}
