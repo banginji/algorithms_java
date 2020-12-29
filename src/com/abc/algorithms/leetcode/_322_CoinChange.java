1b2a591f6f18c4b14eff8da5903ccc4aef3e6ff0
Narayanan Lakshmi <narayanan.lakshmi@tr.com>
2020-12-28 18:49:45 -0800

    322 - coin change dp leet soln

--- src/com/abc/algorithms/leetcode/_322_CoinChange.java
+++ src/com/abc/algorithms/leetcode/_322_CoinChange.java
@@ -1,6 +1,7 @@
 package com.abc.algorithms.leetcode;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class _322_CoinChange {
 
@@ -52,7 +53,63 @@ public class _322_CoinChange {
         return amounts[amount] == Integer.MAX_VALUE ? -1 : amounts[amount];
     }
 
+    private static int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        if (IntStream.of(coins).noneMatch(coin -> amount >= coin)) return -1;
+
+        int result = Integer.MAX_VALUE;
+
+        for (int coin : coins)
+            if (amount >= coin) {
+                int dpRes = coinChange(coins, amount - coin);
+                if (dpRes == -1) continue;
+                result = Math.min(result, dpRes + 1);
+            }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
+    private static int coinChangeDp(int[] coins, int amount) {
+        int[][] dp = new int[amount + 1][coins.length];
+        for (int[] d : dp) Arrays.fill(d, Integer.MAX_VALUE);
+
+        return coinChangeDp(coins, amount, dp);
+    }
+
+    private static int coinChangeDp(int[] coins, int amount, int[][] dp) {
+        if (amount == 0) return 0;
+
+        if (IntStream.of(coins).noneMatch(coin -> amount >= coin)) return -1;
+
+        int result = Integer.MAX_VALUE;
+
+        for (int coinIdx = 0; coinIdx < coins.length; coinIdx++)
+            if (amount >= coins[coinIdx]) {
+                int dpRes;
+                if (dp[amount][coinIdx] != Integer.MAX_VALUE)
+                    dpRes = dp[amount][coinIdx];
+                else
+                    dpRes = dp[amount][coinIdx] = coinChangeDp(coins, amount - coins[coinIdx], dp);
+                if (dpRes == -1) continue;
+                result = Math.min(result, dpRes + 1);
+            }
+
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(coinChanges(new int[]{1, 5, 10, 25}, 10));
+        System.out.println(coinChange(new int[]{1, 2, 5}, 11) == 3);
+        System.out.println(coinChange(new int[]{2}, 3) == -1);
+        System.out.println(coinChange(new int[]{1}, 0) == 0);
+        System.out.println(coinChange(new int[]{1}, 1) == 1);
+        System.out.println(coinChange(new int[]{1}, 2) == 2);
+//        This will timeout
+//        System.out.println(coinChange(new int[]{186, 419, 83, 408}, 6249) == 20);
+
+        System.out.println(coinChangeDp(new int[]{1, 2, 5}, 11) == 3);
+        System.out.println(coinChangeDp(new int[]{2}, 3) == -1);
+        System.out.println(coinChangeDp(new int[]{1}, 0) == 0);
+        System.out.println(coinChangeDp(new int[]{1}, 1) == 1);
+        System.out.println(coinChangeDp(new int[]{1}, 2) == 2);
+        System.out.println(coinChangeDp(new int[]{186, 419, 83, 408}, 6249) == 20);
     }
 }
