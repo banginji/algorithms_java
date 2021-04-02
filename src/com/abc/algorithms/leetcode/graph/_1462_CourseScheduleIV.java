7b603752ee6947201aee4b77ce9da99cc84c6e4f
Narayanan Lakshmi <narayanan.lakshmi@tr.com>
2021-04-01 17:25:20 -0700

    1462 - course schedule IV dfs leet soln

--- src/com/abc/algorithms/leetcode/graph/_1462_CourseScheduleIV.java
+++ src/com/abc/algorithms/leetcode/graph/_1462_CourseScheduleIV.java
@@ -4,78 +4,18 @@ import java.util.*;
 
 public class _1462_CourseScheduleIV {
     private static List<Boolean> courseSchedule(int n, int[][] preReqs, int[][] queries) {
-        Map<Integer, Integer> inDegree = new HashMap<>();
-
-        /**
-         *
-         * inDegree does NOT contain all courses on purpose for this problem
-         *
-         *         for (int course = 0; course < n; course++)
-         *             inDegree.put(course, 0);
-         *
-         * If it is present then it will either appear in front of dependent
-         * courses or after which will lead to inconclusive results
-         *
-         */
-        for (int[] preReq : preReqs) {
-            inDegree.putIfAbsent(preReq[0], 0);
-            inDegree.putIfAbsent(preReq[1], 0);
-            inDegree.computeIfPresent(preReq[1], (k, v) -> v + 1);
-        }
-
-        Map<Integer, List<Integer>> depMap = new HashMap<>();
-
-        for (int[] preReq : preReqs)
-            depMap.computeIfAbsent(preReq[0], x -> new ArrayList<>()).add(preReq[1]);
-
-        Queue<Integer> queue = new LinkedList<>();
-
-        inDegree.entrySet().stream()
-                .filter(entry -> entry.getValue() == 0)
-                .map(Map.Entry::getKey)
-                .forEach(queue::offer);
-
-        LinkedHashSet<Integer> courseList = new LinkedHashSet<>();
-
-        while (queue.size() > 0) {
-            int currentCourse = queue.poll();
-
-            courseList.add(currentCourse);
-
-            for (Integer course : depMap.getOrDefault(currentCourse, Collections.emptyList())) {
-                inDegree.computeIfPresent(course, (k, v) -> v - 1);
-                if (inDegree.getOrDefault(course, -1) == 0)
-                    queue.offer(course);
-            }
-        }
-
+        Map<Integer, List<Integer>> preReqMap = new HashMap<>();
+        for (int[] preReq : preReqs) preReqMap.computeIfAbsent(preReq[0], x -> new ArrayList<>()).add(preReq[1]);
         List<Boolean> result = new ArrayList<>();
-
-        for (int[] query : queries) {
-            int preReqCourse = query[0];
-            int depCourse = query[1];
-
-            /**
-             * If either of the course is NOT found in course list then add 'false'
-             * for that query
-             */
-            if (!courseList.contains(preReqCourse) || !courseList.contains(depCourse)) {
-                result.add(false);
-                continue;
-            }
-
-            for (Integer course : courseList)
-                if (course == preReqCourse || course == depCourse)
-                    if (course == depCourse) {
-                        result.add(false);
-                        break;
-                    } else {
-                        result.add(true);
-                        break;
-                    }
-        }
-
+        for (int[] query : queries) result.add(dfs(query[0], query[1], preReqMap));
         return result;
+    }
+
+    private static boolean dfs(int startCourse, int endCourse, Map<Integer, List<Integer>> preReqMap) {
+        if (startCourse == endCourse) return true;
+        for (Integer course : preReqMap.getOrDefault(startCourse, Collections.emptyList()))
+            if (dfs(course, endCourse, preReqMap)) return true;
+        return false;
     }
 
     public static void main(String[] args) {
